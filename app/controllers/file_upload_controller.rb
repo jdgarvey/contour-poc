d3c4b82197e5ae7f9abccc257c6455c9eff6cd95
Jeff Nickoloff <jeff@allingeek.com>
2015-05-14 02:37:16 +0000

    initial commit

---
+++ app/controllers/file_upload_controller.rb
@@ -0,0 +1,5 @@
+class FileUploadController < ApplicationController
+  def upload
+    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
+  end
+end
